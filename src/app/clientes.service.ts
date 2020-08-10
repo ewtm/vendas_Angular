687102aeb94d7ecb2b83eec3800aabf2d4e8d22b
Eduardo Mergulhao <ewtm@msn.com>
2020-08-10 13:29:09 -0300

    listagem e tabela ok

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -15,6 +15,18 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8080/sistema-vendas/api/clientes', cliente);
   }
 
+  getClientes() : Observable<Cliente[]>{
+    return this.http.get<Cliente[]>('http://localhost:8080/sistema-vendas/api/clientes')
+  }
+
+  getClientesTest() : Cliente[]{
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'Fulano';
+    cliente.dataCadastro = '18/04/2020';
+    cliente.cpf = '123456789';
+    return[cliente]
+  }
 
   
 
